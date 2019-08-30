ebfe87f104b71482a7014e3988892cf53d0fb405
Sirax19 <prokop4uk19@gmail.com>
2019-08-30 18:32:43 +0300

    v-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import * as $ from 'jquery';
+import * as bootstrap from "bootstrap";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
